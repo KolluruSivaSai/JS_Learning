7ba94ec412473c8ff7d265eb8aabf90e1714cc94
Siva Sai Kolluru <77961627+KolluruSivaSai@users.noreply.github.com>
2025-02-07 05:14:42 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId= 144553
+let accountEmail= "sivaijb@gmail.com"
+var accountpwd="12345"
+accountCity = "Vizianagaram"
+let accountState
+//accountId =2 not allowed
+
+/*
+prefer not to use var bcoz of block scoping
+*/
+accountEmail= "dihdgi@gmail.com"
+accountpwd="1333"
+accountCity="Hyd"
+console.log(accountId);
+console.table([accountId, accountEmail,accountpwd,accountState,accountCity])
