232566db561511b5c9d8fa074fb95f53de659eba
Siva Sai Kolluru <77961627+KolluruSivaSai@users.noreply.github.com>
2025-02-07 05:14:16 +0000

    datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,19 @@
+"use strict"// treat all js code as newer version
+
+//alert(3+3)//we are using nodejs , not browser
+
+let name ="siva"
+let age =25
+let isLoggedIn = false
+let state
+//number => 2^53
+//bigint
+//string => " "
+//boolean =>true/false
+//null=>standalone value
+//undefined =>
+//symbol =>uniqueness
+//object
+
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object
